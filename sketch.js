c5ff5756006f9a520e8de6ab9055f76265eccd58
navneet02 <111250275+navneet02@users.noreply.github.com>
2023-07-30 21:04:56 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var bg, bg_img,lander,asteroid1,asteroid2, m1,m_img
+
+
+
+
+function preload(){
+
+
+}
+
+function setup() {
+ createCanvas(2000,2000)
+ bg= createSprite(displayWidth,displayHeight)
+}
+
+function draw() {
+ background(0)
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+    drawSprites()
+}
